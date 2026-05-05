fa7408add68af5a9728447b043bfe644d0ebfc55
Khalil Jackson <jacksonkhalil05@gmail.com>
2026-05-04 22:41:08 -0400

    allow site to run

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
 const port = 3000;
+app.use(express_1.default.static(__dirname));
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html'); // send HTML file on GET request
+    res.sendFile(path_1.default.join(__dirname, 'index.html')); // send HTML file on GET request
 });
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
--- app.ts
+++ app.ts
@@ -1,10 +1,13 @@
 import express, { Request, Response } from 'express';
+import path from 'path';
 
 const app = express();
 const port: number = 3000;
 
+app.use(express.static(__dirname));
+
 app.get('/', (req: Request, res: Response) => {
-    res.sendFile(__dirname + '/index.html'); // send HTML file on GET request
+    res.sendFile(path.join(__dirname, 'index.html')); // send HTML file on GET request
 });
 
 app.listen(port, () => {
